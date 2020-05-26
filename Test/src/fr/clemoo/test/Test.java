e9cdedd61c8bdabc5a41e62d0e9ec86fe7f17917
Clem <clem.accart@laposte.net>
2020-05-26 22:38:31 +0200

    First Commit

---
+++ Test/src/fr/clemoo/test/Test.java
@@ -0,0 +1,21 @@
+package fr.clemoo.test;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world!");
+		
+		int[]tab = new int[10];
+		System.out.println(tab.length);
+		for (int i : tab) {
+			System.out.println(i);
+		}
+		String [] tableau = new String[10];
+		for (String string : tableau) {
+			System.out.println(string);
+		}
+		
+	}
+
+}
